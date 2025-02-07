0505b1029c67ee3924da198d05a0ebbb577f6dd1
OranFlanagan <163148816+OranFlanagan@users.noreply.github.com>
2025-02-07 22:09:37 +0000

    Update ex1.ts

--- ex1.ts
+++ ex1.ts
@@ -1,4 +1,5 @@
 let myArray: string[]=[];
+//creates an empty array
 
 function addTask(task:string):number
 {
@@ -6,6 +7,8 @@ function addTask(task:string):number
     console.log(task+" has been added to my task");
     return myArray.length;
 }
+//the function addTaask adds a string to the already created array myArray
+//then prints the task onto the array
 
 function listAllTasks():void
 {
@@ -13,13 +16,18 @@ function listAllTasks():void
     console.log("Item: "+ele+ " is on my tasks"); 
     })
 }
-
+//when the function listAllTasks is called it iterates over the elements inside the array myArray
+//for whatever is inside the array it adds a message including whatever is inside it
 function deleteTask(task:string):number
 {
     let index:number = myArray.indexOf(task);
+    //this code looks for task inside the myArray array
+    //if a task is found it makes "index" a number
+    //but if not it makes it -1
     if(index > -1)
     {
         myArray.splice(index,1)
+        //splice removes the index at number 1
         console.log("Item: "+task+"has been removed from my Tasks list.");
     }
     else
@@ -31,5 +39,7 @@ function deleteTask(task:string):number
 
 addTask("Work");
 addTask("Eat");
+//add work and eat to task
 listAllTasks();
 deleteTask("Sleep");
+//gets rid of sleep which does not exist
